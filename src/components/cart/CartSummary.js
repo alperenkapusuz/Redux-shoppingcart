83c117040243128d064eefacf6de70f51ea3aa47
Alperen Kapusuz <sefalperen21@gmail.com>
2021-08-06 21:19:46 +0300

    sepetten ürün silinmesi

--- src/components/cart/CartSummary.js
+++ src/components/cart/CartSummary.js
@@ -10,8 +10,8 @@ import {
 } from "reactstrap";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-// import * as cartActions from "../../redux/actions/cartActions";
-// import {Link} from "react-router-dom"
+import * as cartActions from "../../redux/actions/cartActions";
+//import {Link} from "react-router-dom"
 import alertify from "alertifyjs"
 
 class CartSummary extends Component {
@@ -35,14 +35,14 @@ class CartSummary extends Component {
         <DropdownMenu right>
           {this.props.cart.map(cartItem => (
             <DropdownItem key={cartItem.product.id}>
-              {/* <Badge color="danger" onClick={()=>this.removeFromCart(cartItem.product)}>-</Badge> */}
+              <Badge color="danger" onClick={()=>this.removeFromCart(cartItem.product)}>-</Badge>
               {cartItem.product.productName}
               <Badge color="success">{cartItem.quantity}</Badge>
             </DropdownItem>
           ))}
 
           <DropdownItem divider />
-          {/* <DropdownItem><Link to={"/cart"}>Sepete git</Link></DropdownItem> */}
+          <DropdownItem>Sepete git</DropdownItem>
         </DropdownMenu>
       </UncontrolledDropdown>
     );
@@ -56,13 +56,13 @@ class CartSummary extends Component {
   }
 }
 
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     actions: {
-//       removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch)
-//     }
-//   };
-// }
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch)
+    }
+  };
+}
 function mapStateToProps(state) {
   return {
     cart: state.cartReducer
@@ -70,5 +70,6 @@ function mapStateToProps(state) {
 }
 export default connect(
   mapStateToProps,
-//   mapDispatchToProps
-)(CartSummary);
+  mapDispatchToProps
+)(CartSummary)
+
