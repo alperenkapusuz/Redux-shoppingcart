470a5678819e6e19d3755ed1edcfec2c313557d8
Alperen Kapusuz <sefalperen21@gmail.com>
2021-08-06 19:56:10 +0300

    sepete ekleme (react)

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,28 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import { Badge } from "reactstrap";
+import { Badge, Button } from "reactstrap";
 import { bindActionCreators } from "redux";
 import * as productActions from "../../redux/actions/productActions";
-import { Table } from 'reactstrap';
+import * as cartActions from "../../redux/actions/cartActions";
+import { Table } from "reactstrap";
+import alertify from "alertifyjs"
+//import {Link} from "react-router-dom"
 
 class ProductList extends Component {
   componentDidMount() {
     this.props.actions.getProducts();
   }
+  addToCart = (product)=>{
+    this.props.actions.addToCart({quantity:1,product})
+    alertify.success(product.productName + " sepete eklendi")
+  }
   render() {
     return (
       <div>
         <h3>
-          {/* <Badge color="dark">
-            
+          {/* <Badge color="warning">Products</Badge>
+          <Badge color="success">
+            {this.props.currentCategory.categoryName}
           </Badge> */}
-          Products-
-          {/* <Badge color="success">
-            
-          </Badge> */}
-          {this.props.currentCategory.categoryName}
+          Products-{this.props.currentCategory.categoryName}
         </h3>
-
         <Table>
           <thead>
             <tr>
@@ -30,18 +33,23 @@ class ProductList extends Component {
               <th>Product Name</th>
               <th>Unit Price</th>
               <th>Quantity Per Unit</th>
-              <th>Unit In Stock</th>
+              <th>Units In Stock</th>
               <th />
             </tr>
           </thead>
           <tbody>
-            {this.props.products.map((product) => (
+            {this.props.products.map(product => (
               <tr key={product.id}>
                 <th scope="row">{product.id}</th>
                 <td>{product.productName}</td>
                 <td>{product.unitPrice}</td>
                 <td>{product.quantityPerUnit}</td>
                 <td>{product.unitsInStock}</td>
+                <td>
+                  <Button color="success" onClick={()=>this.addToCart(product)}>
+                    ekle
+                  </Button>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -54,7 +62,7 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
-    products: state.ProductListReducer,
+    products: state.productListReducer
   };
 }
 
@@ -62,8 +70,12 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       getProducts: bindActionCreators(productActions.getProducts, dispatch),
-    },
+      addToCart: bindActionCreators(cartActions.addToCart, dispatch)
+    }
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductList);
--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'alertifyjs/build/css/alertify.min.css';
 import {Provider} from "react-redux";
 import configureStore from "./redux/reducers/configureStore"
 
 const store = configureStore();
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}><App /></Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
