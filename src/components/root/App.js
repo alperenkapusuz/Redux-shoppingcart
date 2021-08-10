6c8cc24e01c2c29df1bbf9a41a7dd7ea005ed4ea
Alperen Kapusuz <sefalperen21@gmail.com>
2021-08-10 14:08:24 +0300

    detay sayfaları yazıldı

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -3,6 +3,7 @@ import { Switch,Route } from "react-router-dom";
 import { Container } from "reactstrap";
 import CartDetail from "../cart/CartDetail";
 import Navi from "../navi/Navi";
+import AddOrUpdateProduct from "../products/AddOrUpdateProduct";
 import Dashboard from "./Dashboard";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Dashboard}/>
         <Route path="/product" exact component={Dashboard}/>
+        <Route path="/saveproduct/:productId" exact component={AddOrUpdateProduct}/>
         <Route path="/cart" exact component={CartDetail}/>
       </Switch>
     </Container>
