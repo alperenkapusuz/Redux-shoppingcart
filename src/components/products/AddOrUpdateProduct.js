8361f51771b3d023fc083af2582b005624a93e5a
Alperen Kapusuz <sefalperen21@gmail.com>
2021-08-11 12:19:08 +0300

    validate eklendi

--- src/components/products/AddOrUpdateProduct.js
+++ src/components/products/AddOrUpdateProduct.js
@@ -14,6 +14,7 @@ function AddOrUpdateProduct({
   ...props
 }) {
   const [product, setProduct] = useState({ ...props.product });
+  const [errors, setErrors] = useState({});
   useEffect(() => {
     if (categories.length === 0) {
       getCategories();
@@ -27,6 +28,22 @@ function AddOrUpdateProduct({
       ...previousProduct,
       [name]: name === "categoryId" ? parseInt(value, 10) : value,
     }));
+
+    validate(name,value);
+  }
+
+  function validate(name,value) {
+    if (name === "productName" && value === "") {
+      setErrors((previousErrors) => ({
+        ...previousErrors,
+        productName: "Ürün ismi olmalıdır",
+      }));
+    }else{
+      setErrors((previousErrors) => ({
+        ...previousErrors,
+        productName: "",
+      }));
+    }
   }
 
   function handleSave(event) {
@@ -42,6 +59,7 @@ function AddOrUpdateProduct({
       categories={categories}
       onChange={handleChange}
       onSave={handleSave}
+      errors={errors}
     />
   );
 }
@@ -62,8 +80,6 @@ function mapStateToProps(state, ownProps) {
     products: state.productReducer,
     categories: state.categoryListReducer,
   };
-
-
 }
 
 const mapDispatchToProps = {
